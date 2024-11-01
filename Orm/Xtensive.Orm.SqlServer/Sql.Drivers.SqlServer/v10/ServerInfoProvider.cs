c30dc1cabcacebcec3b3776564e10326de3cea6e
Anton Guschin <anton.guschin@aliexpress.ru>
2024-11-01 15:49:08 +0300

    mssql_date_support

--- Orm/Xtensive.Orm.SqlServer/Sql.Drivers.SqlServer/v10/ServerInfoProvider.cs
+++ Orm/Xtensive.Orm.SqlServer/Sql.Drivers.SqlServer/v10/ServerInfoProvider.cs
@@ -34,9 +34,10 @@ namespace Xtensive.Sql.Drivers.SqlServer.v10
         new ValueRange<DateTimeOffset>(new DateTimeOffset(1, 1, 1, 0, 0, 0, 0, new TimeSpan(0)),
           new DateTimeOffset(9999, 12, 31, 0, 0, 0, 0, new TimeSpan(0))),
         "datetimeoffset");
-      types.DateTime = DataTypeInfo.Range(SqlType.DateTime, common | index,
+      types.DateTime = DataTypeInfo.Fractional(SqlType.DateTime, common | index,
         new ValueRange<DateTime>(new DateTime(1, 1, 1), new DateTime(9999, 12, 31)),
-        "datetime2", "datetime", "date", "smalldatetime");
+        27,
+        "datetime2", "datetime", "smalldatetime", "date");
       
       types.DateOnly = DataTypeInfo.Empty;
 
